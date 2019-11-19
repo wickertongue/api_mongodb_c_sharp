1313b0f0341be70d706d38ee9756aa8b1b0384d2
hayden <haydenwb@gmail.com>
2019-11-19 20:40:11 +0000

    added Product model, now to add database context

---
+++ code_test_c_sharp/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductApi.Models
+{
+    public class Product
+    {
+        public long Id { get; set; }
+        public int ProductCode { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+}

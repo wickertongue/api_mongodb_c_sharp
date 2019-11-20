1d466868d51047d1b7d6e401bf8ac52c37fe8560
hayden <haydenwb@gmail.com>
2019-11-20 07:00:51 +0000

    created services for product

--- code_test_c_sharp/Services/ProductService.cs
+++ code_test_c_sharp/Services/ProductService.cs
@@ -1,10 +1,40 @@
-﻿using MongoDB.Driver;
+﻿using System.Collections.Generic;
+using MongoDB.Driver;
 using ProductApi.Models;
 
-public ProductService(IInventoryDatabaseSettings settings)
+namespace ProductApi.Services
 {
-    var client = new MongoClient(settings.ConnectionString);
-    var database = client.GetDatabase(settings.DatabaseName);
+    public class ProductService
+    {
+        private readonly IMongoCollection<Product> _products;
 
-    _products = database.GetCollection<Product>(settings.InventoryCollectionName);
+        public ProductService(IInventoryDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _products = database.GetCollection<Product>(settings.InventoryCollectionName);
+        }
+
+        public List<Product> Get() =>
+            _products.Find(Product => true).ToList();
+
+        public Product Get(string id) =>
+            _products.Find<Product>(Product => Product.Id == id).FirstOrDefault();
+
+        public Product Create(Product product)
+        {
+            _products.InsertOne(product);
+            return product;
+        }
+
+        public void Update(string id, Product productIn) =>
+            _products.ReplaceOne(product => product.Id == id, productIn);
+
+        public void Remove(Product productIn) =>
+            _products.DeleteOne(product => product.Id == productIn.Id);
+
+        public void Remove(string id) =>
+            _products.DeleteOne(product => product.Id == id);
+    }
 }
